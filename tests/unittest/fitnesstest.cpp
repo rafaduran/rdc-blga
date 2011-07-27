98bf4923ed1c899d01f6f3c2a6f6a36b5788d46a
Rafael Duran Castaneda <rafadurancastaneda@gmail.com>
2011-07-28 00:52:37 +0200

    Added firs dummy fitness test

---
+++ tests/unittest/fitnesstest.cpp
@@ -0,0 +1,45 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2011  <copyright holder> <email>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <gtest/gtest.h>
+#include "PeriodicEqual.h"
+
+/*template <typename E> // E is the element type.
+class Queue {
+ public:
+  Queue();
+  void Enqueue(const E& element);
+  E* Dequeue(); // Returns NULL if the queue is empty.
+  size_t size() const;
+  ...
+};*/
+
+class FitnessTest : public testing::Test
+{
+   public:
+      PeriodicEqual *peObject; 
+    FitnessTest(){
+      peObject = new PeriodicEqual(10);
+    }
+};
+
+
+TEST_F(FitnessTest, testDim)
+{
+  EXPECT_EQ(peObject->getDim(), 10);
+}
