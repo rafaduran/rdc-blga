738035dc45a482d9b17cd9e3e3eb062cf3fdfce8
Rafael Duran Castaneda <rafadurancastaneda@gmail.com>
2011-07-29 00:34:09 +0200

    Changed unittest/fitnesstest.cpp, supporting now generic fitness classes tests. Added some patterns to ignore on gitignore.

--- tests/unittest/fitnesstest.cpp
+++ tests/unittest/fitnesstest.cpp
@@ -17,29 +17,67 @@
 */
 
 #include <gtest/gtest.h>
-#include "PeriodicEqual.h"
+#include "FitnessFunction.h"
 
-/*template <typename E> // E is the element type.
-class Queue {
+#define SIZE P1_P2_dimension
+
+using ::testing::TestWithParam;
+using ::testing::Values;
+
+// As a general rule, to prevent a test from affecting the tests that come
+// after it, you should create and destroy the tested objects for each test
+// instead of reusing them.  In this sample we will define a simple factory
+// function for PrimeTable objects.  We will instantiate objects in test's
+// SetUp() method and delete them in TearDown() method.
+typedef FitnessFunction* CreateFitnessFunc();
+
+template <size_t functionNumber>
+FitnessFunction* CreateFitness() {
+  return  FitnessFunction::getFitnessFunction(functionNumber);
+}
+
+// Inside the test body, fixture constructor, SetUp(), and TearDown() you
+// can refer to the test parameter by GetParam().  In this case, the test
+// parameter is a factory function which we call in fixture's SetUp() to
+// create and store an instance of PrimeTable.
+class FitnessTest : public TestWithParam<CreateFitnessFunc*> {
  public:
-  Queue();
-  void Enqueue(const E& element);
-  E* Dequeue(); // Returns NULL if the queue is empty.
-  size_t size() const;
-  ...
-};*/
-
-class FitnessTest : public testing::Test
-{
-   public:
-      PeriodicEqual *peObject; 
-    FitnessTest(){
-      peObject = new PeriodicEqual(10);
+  virtual ~FitnessTest() { delete ff_; }
+  virtual void SetUp() { 
+    ff_ = (*GetParam())(); 
+    ind1 = new char[SIZE];
+    ind2 = new char[SIZE];
+    for(int i = 0; i < SIZE; i++){
+      ind1[i] = 0;
+      ind2[i] = 1;
     }
+  }
+  virtual void TearDown() {
+    //I don't need deleting ff_ since FitnessFunction already does
+    ff_ = NULL;
+    delete ind1;
+    delete ind2;
+  }
+
+ protected:
+  FitnessFunction* ff_;
+  char *ind1;
+  char *ind2;
+  
 };
 
-
-TEST_F(FitnessTest, testDim)
-{
-  EXPECT_EQ(peObject->getDim(), 10);
+TEST_P(FitnessTest, GetDim) {
+  EXPECT_EQ(ff_->getDim(), P1_P2_dimension);
 }
+
+TEST_P(FitnessTest, FitnessCompare) {
+  ASSERT_LT(ff_->compare(0,1), 0);
+  ASSERT_GT(ff_->compare(1,0), 0);
+  ASSERT_EQ(ff_->compare(1,1), 0);
+}
+
+// factory functions:
+INSTANTIATE_TEST_CASE_P(
+    P1P2,
+    FitnessTest,
+    Values(&CreateFitness<1>, &CreateFitness<0>));
