106f6f62a6524443e9ab900d27e8ad65e4ea2b10
Rafael Duran Castaneda <rafadurancastaneda@gmail.com>
2011-08-03 00:27:19 +0200

    More fitness tests added

--- tests/unittest/fitnesstest.cpp
+++ tests/unittest/fitnesstest.cpp
@@ -18,6 +18,8 @@
 
 #include <gtest/gtest.h>
 #include "FitnessFunction.h"
+#include "fitnesstest.h"
+#include <math.h>
 
 #define SIZE P1_P2_dimension
 
@@ -38,31 +40,27 @@ class FitnessTest : public TestWithParam<CreateFitnessFunc*> {
  public:
   virtual ~FitnessTest() { delete ff_; }
   virtual void SetUp() { 
-    ff_ = (*GetParam())(); 
-    ind1 = new char[SIZE];
-    ind2 = new char[SIZE];
-    for(int i = 0; i < SIZE; i++){
-      ind1[i] = 0;
-      ind2[i] = 1;
-    }
+    ff_ = (*GetParam())();
+    _ind1 = new char[SIZE];
+    _ind2 = new char[SIZE];
   }
   virtual void TearDown() {
     //I don't need deleting ff_ since FitnessFunction already does
     ff_ = NULL;
-    delete ind1;
-    delete ind2;
+    delete _ind1;
+    delete _ind2;
   }
 
  protected:
   FitnessFunction* ff_;
-  char *ind1;
-  char *ind2;
-  
+  const char *aux;
+  char *_ind1;
+  char *_ind2;
 };
 
 TEST_P(FitnessTest, GetDim) {
   
-  if(strcmp(ff_->getName().str().data(), "Hump function")) {
+  if(ff_->getFunctionNumber() != 2) {
     EXPECT_EQ(ff_->getDim(), P1_P2_dimension);  // Not Hump function
   }
   else {
@@ -76,6 +74,68 @@ TEST_P(FitnessTest, FitnessCompare) {
   ASSERT_GT(ff_->compare(1,0), 0);
   ASSERT_EQ(ff_->compare(1,1), 0);
 }
+
+TEST_P(FitnessTest, FunctionNumber){
+  switch(ff_->getFunctionNumber()){
+    case 0:
+      ASSERT_STREQ(ff_->getName().str().data(), "Periodic equal");
+      break;
+    case 1:
+      ASSERT_STREQ(ff_->getName().str().data(), "Periodic unequal");
+      break;
+    case 2:
+      ASSERT_STREQ(ff_->getName().str().data(), "Hump function");
+      break;
+  }
+}
+
+TEST_P(FitnessTest, Inversegray){
+  /* Test values can be obtained from online gray/binary converter at:
+     http://www.strw.leidenuniv.nl/~mathar/progs/gray.html */
+  char *binary = new char[SIZE];
+  for(int j = 0; j < 3; j++){
+    for(int i = 0; i < SIZE; i++ ){
+      switch(j){
+        case 0:
+          _ind1[i] = 1;             // Gray   111111111111111111111111111111 
+          _ind2[i] = 1 - (i % 2);   // Binary 101010101010101010101010101010
+          break;
+        case 1:
+          _ind1[i] = 0;   // Gray   000000000000000000000000000000
+          _ind2[i] = 0;   // Binary 000000000000000000000000000000
+          break;
+        case 2:
+          _ind1[i] = 1;             // Gray   110111101111111101110111100011 
+          _ind2[i] = 0;             // Binary 100101001010101001011010111101
+          break;
+      }
+    }
+    if(j == 2){
+      _ind1[2] = _ind1[7] = _ind1[16] = _ind1[20] = _ind1[25] = _ind1[26] = \
+        _ind1[27] = 0; 
+      _ind2[0] = _ind2[3] = _ind2[5] = _ind2[8] = _ind2[10] = _ind2[12] =\
+        _ind2[14] = _ind2[17] = _ind2[19] = _ind2[20] = _ind2[22] = _ind2[24] =\
+        _ind2[25] = _ind2[26] = _ind2[27] = _ind2[29] = 1;
+    }
+
+    if(ff_->getFunctionNumber()!=2){
+      ff_->inverseGray(_ind1, binary); // 
+      for(int i = 0; i < SIZE; i++ )
+        ASSERT_EQ(int(binary[0]), int(_ind2[0]));
+    } // TODO: test inverseGrayVector from Hump function   
+  }
+  delete binary;
+}
+
+/*TEST_P(FitnessTest, Distance) {
+  ASSERT_EQ(ff_->distance(ff_->inverseGray(_ind1),
+    ff_->inverseGray(_ind2)), );
+  ASSERT_FALSE(ff_->distance(_ind1,_ind1));
+  ASSERT_FALSE(ff_->distance(_ind2,_ind2));
+  for(int i = 0; i < SIZE/2; i++)
+    _ind1[i] = 1;
+  ASSERT_NEAR(ff_->distance(_ind1, _ind2), 0.5, 1 / pow(2,SIZE-1));
+}*/
 
 // factory functions:
 INSTANTIATE_TEST_CASE_P(
