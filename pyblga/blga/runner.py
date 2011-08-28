57904efd201c23f1e1be5aeab8261df59fabda8f
Rafael Duran Castaneda <rafadurancastaneda@gmail.com>
2011-08-28 12:38:56 +0200

    Ignoring wild import warining

--- pyblga/blga/runner.py
+++ pyblga/blga/runner.py
@@ -15,7 +15,7 @@ using a `subprocess` moudule
 from __future__ import unicode_literals
 from __future__ import absolute_import
 from __future__ import print_function
-from future_builtins import *
+from future_builtins import * #@UnusedWildImport
 
 import subprocess
 import os
