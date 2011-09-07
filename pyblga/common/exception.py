5ebc470b5441649c726c69c8cd2940b8d48811dc
Rafael Duran Castaneda <rafadurancastaneda@gmail.com>
2011-09-08 00:14:16 +0200

    Added handlers for data models API errors on pyblga.common.exception

--- pyblga/common/exception.py
+++ pyblga/common/exception.py
@@ -11,11 +11,28 @@ Long description
     
 .. moduleauthor::"Rafael Durán Castañeda <rafadurancastaneda@gmail.com>"
 """
+import sqlalchemy.exc as exc
 import pyblga.common.rdc_utils as utils
 
+
+def raiser(error, msg=None):
+    if msg:
+        raise ModelsAPIError(msg)
+    raise ModelsAPIError(error.message)
+
+
+class ModelsAPIError(exc.SQLAlchemyError):
+    def __init__(self, msg=None):
+        msg="Data: Error on data models API: {0}".format(msg)
+        super(ModelsAPIError, self).__init__(msg)
+
+
+# Error parser decorators
 parsing_error_wrapper = utils.error_wrapper(
     errors=(ValueError, TypeError, KeyError),
     msg="Blga:Error while parsing Blga args",
     error_func=lambda x,y: (False, ':'.join((y, x.message)))
     )
-    
+
+data_error_wrapper = utils.error_wrapper(errors=exc.SQLAlchemyError,
+        msg=None, error_func=raiser)
