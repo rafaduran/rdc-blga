e49d32022049b516d9e6a1ca74615203d082d5d1
Rafael Duran Castaneda <rafadurancastaneda@gmail.com>
2011-09-11 11:49:41 +0200

    Updated tests sample data

--- pyblga/tests/sampledata.py
+++ pyblga/tests/sampledata.py
@@ -18,23 +18,32 @@ sys.path.append(os.path.abspath(os.path.join(os.path.abspath(__file__), '..',
 import pyblga.data.apis as apis
 
 def load_data():
+    result_model = apis.results.ResultsAPI()
     for data in ({'0': '0.99'}, {'1': '0.5'}, {'2': '0.33'}):
-        result_model = apis.results.ResultsAPI()
         result_model.create({'data': data})
-        
+    
+    searcher_model = apis.searchers.SearchersAPI()
     for name in ('Blga', 'Blga', 'ClusteredClearing'):
-        searcher_model = apis.searchers.SearchersAPI()
-        searcher_model.create({'name': name})
+        if name == 'ClusteredClearing':
+            searcher_model.create({'name': name, 'tag': 'tag'})
+        else:
+            searcher_model.create({'name': name})
         
-    for name, value in (('popSize', 500), ('popSize', 300), ('pamnass', 5)):
-        param_model = apis.parameters.ParamsAPI()  
+    param_model = apis.parameters.ParamsAPI()      
+    for name, value in (('popSize', 500), ('pamnass', 5), ('popSize', 300)):
         param_model.create({'name': name, 'value': value})
-        
+    
+    run_model = apis.runs.RunsAPI()
     for index in range(1,4):
-        run_model = apis.runs.RunsAPI()
         run_model.create({'searcher_id': index ,'result_id': index})
-        for param in range(1,4):
+        if index == 3:
+            start, end = 2, 4
+        else:
+            start, end = 1, 3
+        for param in range(start,end):
             run_model.add_param({'run_id': index, 'param_id': param})
+            
+    
         
 if __name__ == '__main__':
     load_data()
