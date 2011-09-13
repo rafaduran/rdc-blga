126683c2bdc3b48953236b778d6049417e6b477d
Rafael Duran Castaneda <rafadurancastaneda@gmail.com>
2011-09-14 00:42:07 +0200

    Added ResultsTableModel to pyblga.gui.modesl for M/V/D

--- pyblga/gui/models.py
+++ pyblga/gui/models.py
@@ -14,8 +14,7 @@ web page for further information about M/V/D.
 .. moduleauthor::"Rafael Durán Castañeda <rafadurancastaneda@gmail.com>"
 """
 from __future__ import unicode_literals
-#import collections
-
+#import logging
 import PyQt4.QtCore as core
 
 import pyblga.data.apis.runs as runs
@@ -26,6 +25,7 @@ import pyblga.common.exception as error
 import pyblga.common.rdc_utils as utils
 
 #TODO: order models methods
+#TODO: remove property columns, since not needed
   
 class RunsTableModel(core.QAbstractTableModel):
     SEARCHER_NAME = 0
@@ -69,6 +69,12 @@ class RunsTableModel(core.QAbstractTableModel):
     def __del_column(self, index):
         del self._columns__values[index]
         
+        
+    def get_result_id(self, index=None):
+        if not index:
+            return self.runs[0].result_id
+        return self.runs[index.row()].result_id
+    
         
     def rowCount(self, index=core.QModelIndex()):
         return len(self.runs)
@@ -131,4 +137,83 @@ class RunsTableModel(core.QAbstractTableModel):
                 return core.QVariant(self.columns[section-2])
             return core.QVariant()
         return core.QVariant()
+    
+    
+class ResultsTableModel(core.QAbstractTableModel):
+    FITNESS = 0
+    @error.model_error_wrapper
+    def __init__(self):
+        super(ResultsTableModel, self).__init__()
+        self.reAPI = results.ResultsAPI()
+        self.data = []
+        self.cur_run = None
+        self.runs = None
+        self.cur_iter = None 
+        self.iters = None
         
+    
+    @error.model_error_wrapper
+    def load_result(self, result_id):
+        self.data = self.reAPI.get_data(result_id).data
+        self.runs = self.data.keys()
+        self.cur_run = self.runs[0]
+        self.iters = self.data[self.cur_run].keys()
+        self.cur_iter = self.iters[0]
+
+
+    def rowCount(self, index=core.QModelIndex()):
+        if not self.data:
+            return 0
+        return len(self.data[self.cur_run][self.cur_iter])
+    
+    
+    def columnCount(self, index=core.QModelIndex()):
+        if not self.data:
+            return 0
+        return 1+len(self.data[self.cur_run][self.cur_iter][0]['variable'])
+
+    
+    def flags(self, index):
+        return super(ResultsTableModel,self).flags(index)
+    
+    
+    def data(self, index, role=core.Qt.DisplayRole):
+        if not self.data:
+            return core.QVariant()
+        if(not index.isValid()) or not (0 <= index.row() < \
+            len(self.data[self.cur_run][self.cur_iter])):
+            return core.QVariant()
+        row = index.row()
+        col = index.column()
+        if role == core.Qt.DisplayRole:
+            if col == ResultsTableModel.FITNESS:
+                return core.QVariant(\
+                    self.data[self.cur_run][self.cur_iter][row]['fitness'])
+            return core.QVariant(self.data[self.cur_run][self.cur_iter][row]\
+                                 ['variable'][str(col-1)])
+            return core.QVariant()
+        elif role == core.Qt.TextAlignmentRole:
+            return core.QVariant(int(core.Qt.AlignCenter))
+        elif role == core.Qt.ToolTipRole:
+            if col == ResultsTableModel.FITNESS:
+                return core.QVariant("Fitness")
+            elif 1 <= col < len(self.data[self.cur_run][self.cur_iter][0]):
+                return core.QVariant("Variable {0}".format(col-1))
+            return core.QVariant()
+        return core.QVariant()
+    
+    
+    def headerData(self, section, orientation, role=core.Qt.DisplayRole):
+        if not self.data:
+            return core.QVariant()
+        if role == core.Qt.TextAlignmentRole:
+            if orientation == core.Qt.Horizontal:
+                return core.QVariant(int(core.Qt.AlignLeft|core.Qt.AlignCenter))
+            return core.QVariant(int(core.Qt.AlignRight|core.Qt.AlignCenter))
+        elif role == core.Qt.DisplayRole and orientation == core.Qt.Horizontal:
+            if section == ResultsTableModel.FITNESS:
+                return core.QVariant("Fitness")
+            elif 1 <= section < len(self.data[self.cur_run][self.cur_iter][0]):
+                return core.QVariant("Variable {0}".format(section-1))
+            return core.QVariant()
+                
